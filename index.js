88dd33a43c53dccbd9c5c2a9cefe7fc874134725
Hunter <Huntermatyi@gmail.com>
2023-12-10 19:18:59 -0500

    donezo

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat() {
+    const newcats = cats.slice();
+    newcats.push("Broom");
+    return newcats;
+}
+function prependCat() {
+    const newcats = cats.slice();
+    newcats.unshift("Arnold");
+    return newcats;
+}
+function removeLastCat() {
+    const newcats = cats.slice();
+    newcats.pop();
+    return newcats;
+}
+function removeFirstCat() {
+    const newcats = cats.slice();
+    newcats.shift();
+    return newcats;
+}
